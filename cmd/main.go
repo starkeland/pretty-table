61a867061cac55719d712541115625e945dc813c
starkland <zhangxinshuai@qianxin.com>
2025-01-07 11:36:14 +0800

    add examples

--- cmd/main.go
+++ cmd/main.go
@@ -13,9 +13,10 @@ func main() {
 	// table.SetCaption(" 配置信息")
 	table.SetHeader([]string{"配置项", "值"})
 	table.AddRows([][]string{
-		{"etcd", "etcd://10.59.15.13:2381,10.59.15.14:2381,10.59.15.15:2381/da.skylar"},
-		{"db", "mongo://admin:CVCPF7JUEbnTLr.1_ZAa1@10.59.15.13:27017,10.59.15.14:27017,10.59.15.15:27017/da_skylar_moss?alias=moss&authdb=admin&create_table=true"},
-		{"cache", "redis://:u39DHyOqkP23nt2P_ZAa1@10.59.15.13:16379,10.59.15.13:16380,10.59.15.14:16379,10.59.15.14:16380,10.59.15.15:16379,10.59.15.15:16380"},
+		{"a", "b"},
+		{"c", "d"},
+		{"e", "f"},
+		{"g", "h"},
 	}...)
 	table.Render()
 }
